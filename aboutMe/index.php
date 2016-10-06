c261690f1cc2a4c6c4f93e67888161ccc3fbe34e
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2016-10-06 11:50:29 +0200

    Create index.php

---
+++ aboutMe/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home - Lucas Márquez Muñoz</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+     <!--  <a class="navbar-brand" href="">Portfolio</a> -->
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="../index.php">Home</a></li>
+	      <li><a href="../dwes/index.php">DWES</a></li>
+        <li><a href="../hlc/index.php">HLC</a></li>
+        <li><a href="https://lumamu.blogspot.com">Blog</a></li>
+        <li><a href="../aboutMe/index.php">Contacto</a></li>
+      </ul>     
+    </div>
+  </div>
+</nav>
+
+
+  
+<div class="container">    
+  <div class="row">
+    <div class="col-lg-3">
+    <br>
+      <img src="../images/yo.jpg" class="img-responsive" style="width:350px" alt="Image"></a>
+    </div>
+    <div class="col-lg-5"> 
+      <br>
+      <ul>
+        <li>
+        Nombre: Lucas Márquez Muñoz.
+        </li>
+        <li>
+        <a href="https://github.com/sargentocharli">GitHub</a>
+        </li>
+        <li>
+        <a href="https://lumamu.blogspot.com">Blog</a>
+        </li>
+        <li>
+        Correo electrónico: <a href="mailto:sargentocharli@gmail.com">sargentocharli@gmail.com</a>
+        </li>
+
+      
+    </div>
+    
+</div><br>
+
+</body>
+</html>
+
