5662b6df5dbff91dc4fe66f41c851742db8fdfc9
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2017-02-24 19:03:01 +0100

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,77 @@
+{
+	let botonPrincipal;
+	let botonSobreMi;
+	let botonProyectos;
+	let botonCV;
+	let botonContacto;
+	let botonRojo;
+
+	let mostrarPrincipal = function() {
+		document.getElementById("contenidoMonitor").style.background = "url('imagenes/blanco.png')";
+		document.getElementById("contenidoMonitorPrincipal").style.display = "inline-block";		
+		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
+		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorContacto").style.display = "none";
+
+
+	}
+
+	let mostrarSobreMi = function() {
+		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
+		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
+		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorContacto").style.display = "none";
+		document.getElementById("contenidoMonitor").style.display = "inherit";
+	}
+
+	let mostrarProyectos = function() {
+		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
+		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
+		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorContacto").style.display = "none";
+		document.getElementById("contenidoMonitor").style.display = "inherit";
+	}
+
+	let mostrarCV = function() {
+		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
+		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
+		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorContacto").style.display = "none";
+		document.getElementById("contenidoMonitor").style.display = "inherit";
+	}
+
+	let mostrarContacto = function() {
+		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
+		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
+		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorContacto").style.display = "none";
+		document.getElementById("contenidoMonitor").style.display = "inherit";
+	}
+
+	let mostrarEspacio = function() {
+		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
+		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
+		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorContacto").style.display = "none";
+		document.getElementById("contenidoMonitor").style.height = "	0%";
+
+	}
+
+	let init = function() {
+		botonPrincipal = document.getElementById("botonPrincipal");
+		botonSobreMi = document.getElementById("botonSobreMi");
+		botonProyectos = document.getElementById("botonProyectos");
+		botonCV = document.getElementById("botonCV");
+		botonContacto = document.getElementById("botonContacto");
+		botonRojo = document.getElementById("botonRojo");
+
+		botonPrincipal.addEventListener("click", mostrarPrincipal);
+		botonSobreMi.addEventListener("click", mostrarSobreMi);
+		botonProyectos.addEventListener("click", mostrarProyectos);
+		botonCV.addEventListener("click", mostrarCV);
+		botonContacto.addEventListener("click", mostrarContacto);
+		botonRojo.addEventListener("click", mostrarEspacio);
+	}
+
+	window.onload = init;
+}
