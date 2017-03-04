06a10bf8a3f6c31b4aba4013b1e482ee580adfc2
Lucas Márquez Muñoz <sargentocharli@gmail.com>
2017-03-04 18:30:05 +0100

    Add files via upload

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -6,54 +6,68 @@
 	let botonContacto;
 	let botonRojo;
 
-	let mostrarPrincipal = function() {			
+	let estatica = function() {
+		document.getElementById("contenidoMonitor").style.background = "url('imagenes/estatica.gif')";
+		document.getElementById("contenidoMonitor").style.height = "100%";
+		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
+		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorCV").style.display = "none";
+		document.getElementById("contenidoMonitorContacto").style.display = "none";
+		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
+	}
 
+	let mostrarPrincipal = function() {
 		document.getElementById("contenidoMonitor").style.height = "100%";
 		document.getElementById("contenidoMonitor").style.background = "url('imagenes/blanco.png')";
 		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
 		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorCV").style.display = "none";
 		document.getElementById("contenidoMonitorContacto").style.display = "none";
 		document.getElementById("contenidoMonitorPrincipal").style.display = "inline-block";	
 		document.getElementById("monitorVacio").style.display = "inline-block";
-		
-
-
-
 	}
 
 	let mostrarSobreMi = function() {
 		document.getElementById("contenidoMonitor").style.height = "100%";
+		document.getElementById("contenidoMonitor").style.background = "url('imagenes/blanco.png')";
 		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
-		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
+		document.getElementById("contenidoMonitorSobreMi").style.display = "inline-block";
 		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorCV").style.display = "none";
 		document.getElementById("contenidoMonitorContacto").style.display = "none";
 		document.getElementById("contenidoMonitor").style.display = "inherit";
 	}
 
 	let mostrarProyectos = function() {
 		document.getElementById("contenidoMonitor").style.height = "100%";
+		document.getElementById("contenidoMonitor").style.background = "url('imagenes/blanco.png')";
 		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
 		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
 		document.getElementById("contenidoMonitorProyectos").style.display = "inline-block";
+		document.getElementById("contenidoMonitorCV").style.display = "none";
 		document.getElementById("contenidoMonitorContacto").style.display = "none";
 		document.getElementById("contenidoMonitor").style.display = "inherit";
 	}
 
 	let mostrarCV = function() {
 		document.getElementById("contenidoMonitor").style.height = "100%";
+		document.getElementById("contenidoMonitor").style.background = "url('imagenes/blanco.png')";
 		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
 		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
 		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorCV").style.display = "inline-block";
 		document.getElementById("contenidoMonitorContacto").style.display = "none";
 		document.getElementById("contenidoMonitor").style.display = "inherit";
 	}
 
 	let mostrarContacto = function() {
 		document.getElementById("contenidoMonitor").style.height = "100%";
+		document.getElementById("contenidoMonitor").style.background = "url('imagenes/blanco.png')";
 		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
 		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
 		document.getElementById("contenidoMonitorProyectos").style.display = "none";
-		document.getElementById("contenidoMonitorContacto").style.display = "none";
+		document.getElementById("contenidoMonitorCV").style.display = "none";
+		document.getElementById("contenidoMonitorContacto").style.display = "inline-block";
 		document.getElementById("contenidoMonitor").style.display = "inherit";
 	}
 
@@ -61,9 +75,36 @@
 		document.getElementById("contenidoMonitorPrincipal").style.display = "none";
 		document.getElementById("contenidoMonitorSobreMi").style.display = "none";
 		document.getElementById("contenidoMonitorProyectos").style.display = "none";
+		document.getElementById("contenidoMonitorCV").style.display = "none";
 		document.getElementById("contenidoMonitorContacto").style.display = "none";
 		document.getElementById("contenidoMonitor").style.height = "	0%";
 
+	}
+
+	let mostrarPrincipal2 = function() {
+		estatica();
+		setTimeout(mostrarPrincipal, 500);
+	}
+
+	let mostrarSobreMi2 = function() {
+		estatica();
+		setTimeout(mostrarSobreMi, 500);
+	}
+
+	let mostrarProyectos2 = function() {
+		estatica();
+		setTimeout(mostrarProyectos, 500);
+	}
+
+	let mostrarCV2 = function() {
+		estatica();
+		setTimeout(mostrarCV, 500);
+	}
+
+
+	let mostrarContacto2 = function() {
+		estatica();
+		setTimeout(mostrarContacto, 500);
 	}
 
 	let init = function() {
@@ -74,11 +115,11 @@
 		botonContacto = document.getElementById("botonContacto");
 		botonRojo = document.getElementById("botonRojo");
 
-		botonPrincipal.addEventListener("click", mostrarPrincipal);
-		botonSobreMi.addEventListener("click", mostrarSobreMi);
-		botonProyectos.addEventListener("click", mostrarProyectos);
-		botonCV.addEventListener("click", mostrarCV);
-		botonContacto.addEventListener("click", mostrarContacto);
+		botonPrincipal.addEventListener("click", mostrarPrincipal2);
+		botonSobreMi.addEventListener("click", mostrarSobreMi2);
+		botonProyectos.addEventListener("click", mostrarProyectos2);
+		botonCV.addEventListener("click", mostrarCV2);
+		botonContacto.addEventListener("click", mostrarContacto2);
 		botonRojo.addEventListener("click", mostrarEspacio);
 	}
 
